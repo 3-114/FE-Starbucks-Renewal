3b1921d64e6a18adc6a356368e76327b9495b82a
KIM DONG HYUN <zofhfhguddk@naver.com>
2025-04-01 22:35:57 +0900

    feat: event page searchParams 문제 수정

--- src/app/(main)/(hasNav)/event/page.tsx
+++ src/app/(main)/(hasNav)/event/page.tsx
@@ -1,20 +1,16 @@
-import EventNav from "@/components/layout/navs/EventNav"
 import { EventNavData } from "@/data/NavData"
 
 export default async function Page({
   searchParams,
 }: {
-  searchParams: { category?: string }
+  searchParams: Promise<{ category?: string }>
 }) {
-  const category = searchParams.category ?? EventNavData[0].title
-
+  const params = await searchParams;
+  const category = params.category ?? EventNavData[0].title;
 
   return (
-    <>
-      <EventNav NavData={EventNavData} />
-      <div className="p-6">
-        <h1 className="text-xl font-bold mb-4">{category}</h1>
-      </div>
-    </>
-  )
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">{category}</h1>
+    </div>
+  );
 }
