1e40b81494c146f1a5192d453280d99f181803c2
kimura <dnanf12345@gmail.com>
2025-04-03 10:51:29 +0900

    fix : login page

--- src/components/form/LoginForm.tsx
+++ src/components/form/LoginForm.tsx
@@ -1,10 +1,36 @@
-import { Input } from "@/components/ui/input"
+import NomalSeparator from '@/components/separator/NomalSeparator';
+import BottomButtonWrapper from '../layout/Footers/BottomButtonWrapper';
+
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+import { LoginUnderMenuData } from '@/data/LoginData';
 
 export default function LoginForm() {
   return (
-    <div className="flex flex-col gap-6">
-        <Input name="username" type="text" placeholder="아이디" variant="login" />
-        <Input name="password" type="password" placeholder="비밀번호" variant="login" />
-    </div>
-  )
+    <form className="w-full relative space-y-4">
+      <Input name="username" type="text" placeholder="아이디" variant="login" />
+      <Input
+        name="password"
+        type="password"
+        placeholder="비밀번호"
+        variant="login"
+      />
+      <NomalSeparator
+        data={LoginUnderMenuData}
+        className="pt-8 flex justify-center leading-0 font-semibold whitespace-nowrap"
+      />
+
+      <BottomButtonWrapper>
+        <Button
+          type="submit"
+          variant="largetpye"
+          size="lg"
+          className="w-full text-lg font-bold py-6"
+        >
+          로그인하기
+        </Button>
+      </BottomButtonWrapper>
+    </form>
+  );
 }
--- src/components/shared/WelcomeUserCard.tsx
+++ src/components/shared/WelcomeUserCard.tsx
@@ -9,21 +9,20 @@ export default function WelcomeUserCard({
     greeting = DummyUserInfo.greeting,
     message = DummyUserInfo.message,
   }: WelcomeUserCardProps) {
-  return (
-    <div className="flex flex-col gap-6">
-      <Avatar className="size-[62px]">
-        <AvatarImage src={avatarUrl} />
-        <AvatarFallback>{avatarname?.charAt(0) ?? ''}</AvatarFallback>
-      </Avatar>
-      <div className="w-full flex flex-col gap-3">
-        <div className="font-semibold tracking-tighter text-[22px] gap-[10px]">
-            <h1>{greeting}</h1>
-            <h1>{avatarname}입니다.</h1>
-        </div>
-        <span className="font-[Inter] text-sm font-medium tracking-tighter leading-normal text-[#717171]">
+    return (
+      <section className="space-y-4 pt-15 pb-10">
+        <Avatar className="size-[5rem]">
+          <AvatarImage src={avatarUrl} />
+          <AvatarFallback>{avatarname?.charAt(0) ?? ''}</AvatarFallback>
+        </Avatar>
+        <h1 className="text-3xl font-bold">
+          {greeting}
+          <br />
+          {avatarname}입니다.
+        </h1>
+        <p className="text-md font-medium tracking-tighter leading-normal text-[#717171]">
           {message}
-        </span>
-      </div>
-    </div>
-  )
-}
+        </p>
+      </section>
+    );
+  }
