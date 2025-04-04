3dddd504fc0cb1416964adcabeb5906594086731
kimura <dnanf12345@gmail.com>
2025-04-04 14:05:42 +0900

    fix : tossprivacyconsent-퍼블리싱

--- src/components/shared/PhoneCertification.tsx
+++ src/components/shared/PhoneCertification.tsx
@@ -13,16 +13,16 @@ import { Input } from '@/components/ui/input';
 
 export default function PhoneCertification() {
   return (
-    <section className="group">
+    <section className="group tracking-tighter">
       <p
-        className="font-[Pretendard] text-[22px] font-semibold
-       leading-[30px] tracking-[-0.02em] text-black pt-16 pb-6"
+        className="font-[Pretendard] text-[22px] font-medium
+       leading-[30px] text-black pt-16 pb-6"
       >
         본인확인을 위해
         <br />
         인증을 진행해 주세요
       </p>
-      <div className="text-sm/normal font-medium tracking-tighter">
+      <div className="text-sm/normal font-semibold tracking-tighter">
         <CheckBoxGroup
           label="[필수] 본인 인증 서비스 약관 전체 동의"
           className=""
@@ -31,26 +31,31 @@ export default function PhoneCertification() {
         <LinkBoxGroup
           label="휴대폰 본인 인증 서비스 이용약관 동의"
           link="/"
-          className=""
+          className="font-medium"
         />
         <LinkBoxGroup
           label="휴대폰 통신사 이용약관 동의"
           link="/"
-          className=""
+          className="font-medium"
         />
         <LinkBoxGroup
           label="개인정보 제공 및 이용 동의"
           link="/"
-          className=""
+          className="font-medium"
         />
-        <LinkBoxGroup label="고유식별정보 처리" link="/" className="" />
+        <LinkBoxGroup
+          label="고유식별정보 처리"
+          link="/"
+          className="font-medium"
+        />
       </div>
       <form action="" className="space-y-4">
+        <hr className="border-[1px]" />
         <Input placeholder="이름" variant="login"></Input>
         <Input placeholder="생년월일" variant="login"></Input>
-        <div className="w-full flex items-center">
+        <div className="w-full flex items-center space-x-3">
           <Select defaultValue="skt">
-            <SelectTrigger className="w-[80px] px-0 pt-3 pb-5 justify-between text-sm/normal text-[#6e6e6e] border-none">
+            <SelectTrigger className="w-1/3 px-0 py-4 text-sm/normal text-[#6e6e6e] border-none">
               <SelectValue />
             </SelectTrigger>
             <SelectContent>
@@ -59,13 +64,17 @@ export default function PhoneCertification() {
               <SelectItem value="lg">LG U+</SelectItem>
             </SelectContent>
           </Select>
-          <Input placeholder="전화번호" variant="login"></Input>
+          <Input
+            placeholder="전화번호"
+            variant="login"
+            className="border-none"
+          ></Input>
           <Button
             type="submit"
             variant="navcarousel"
             size="navcarousel"
             color="white"
-            className="py-0"
+            className="py-0 px-0"
           >
             인증요청
           </Button>
@@ -73,7 +82,7 @@ export default function PhoneCertification() {
       </form>
 
       <hr />
-      <ul className="text-sm/normal font-thin tracking-tighter text-[#A0A0A0] list-disc">
+      <ul className="text-sm/normal font-thin tracking-tighter text-[#A0A0A0] list-disc py-5">
         <li>
           타인의 개인정보를 도용하여 가입한 경우, 서비스 이용 제한 및 법적
           제재를 받으실 수 있습니다.
