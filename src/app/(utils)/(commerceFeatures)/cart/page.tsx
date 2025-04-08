3769b9590316cf70ecf8da37c7d6f9eda010bed3
KIM DONG HYUN <zofhfhguddk@naver.com>
2025-04-09 00:06:45 +0900

    wip : 구조에 대한 고찰

--- src/app/(utils)/(commerceFeatures)/cart/page.tsx
+++ src/app/(utils)/(commerceFeatures)/cart/page.tsx
@@ -1,8 +1,9 @@
+import CartPage from "@/components/page/CartPage"
+
 export default function page() {
   return (
     <main>
-      {/* 배송지 캐러셀 */}
-      {/* 일반과 예약 */}
+      <CartPage/>
     </main>
   )
 }
---
+++ src/components/page/CartPage.tsx
@@ -0,0 +1,8 @@
+export default function CartPage() {
+  return (
+    <div>CartPage
+      {/* 배송지 캐러셀 */}
+      {/* 일반과 예약 */}
+    </div>
+  )
+}
