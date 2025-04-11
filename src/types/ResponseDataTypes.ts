f3585d9d80f538b0f31eec539c242b3b760c505b
Jason Ahn <beat1103@gmail.com>
2025-04-11 13:07:09 +0900

    [feat] next-auth and login

--- src/types/ResponseDataTypes.ts
+++ src/types/ResponseDataTypes.ts
@@ -1,3 +1,17 @@
+export interface commonResponseType<T> {
+  httpStatus: string;
+  isSuccess: boolean;
+  message: string;
+  code: number;
+  result: T;
+}
+
+export interface signInDataType {
+  accessToken: string;
+  name: string;
+  uuid: string;
+}
+
 export type NavItem = {
   id: number;
   eventName: string;
