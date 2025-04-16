5ec7e131cf0d38d122cded7fa97445ac6d15a02e
kimura <dnanf12345@gmail.com>
2025-04-16 09:21:24 +0900

    fix : build 에러 수정

--- src/app/(main)/(hasNav)/best/page.tsx
+++ src/app/(main)/(hasNav)/best/page.tsx
@@ -1,13 +1,42 @@
 import BestPage from '@/components/page/BestPage';
+import { EventNavMenuType } from '@/types/Initial/InitialDataTypes';
 
-export default function Page({
+const dummyNavData: EventNavMenuType[] = [
+  {
+    id: 1,
+    eventName: '텀블러/보온병',
+  },
+  {
+    id: 2,
+    eventName: '머그/컵',
+  },
+  {
+    id: 3,
+    eventName: '라이프스타일',
+  },
+  {
+    id: 4,
+    eventName: '티/커피용품',
+  },
+  {
+    id: 5,
+    eventName: '케이크',
+  },
+  {
+    id: 6,
+    eventName: '초콜릿스낵',
+  },
+];
+
+export default async function Page({
   searchParams,
 }: {
-  searchParams: { category?: string };
+  searchParams: Promise<{ category?: string | undefined }>;
 }) {
+  const category = (await searchParams).category ?? dummyNavData[0].eventName;
   return (
-    <div>
-      <BestPage searchParams={searchParams} />
-    </div>
+    <>
+      <BestPage category={category} NavData={dummyNavData} />
+    </>
   );
 }
