2d488e9a87e474b2565c936a450a2eca5de15077
kimura <dnanf12345@gmail.com>
2025-04-17 15:40:06 +0900

    wip : api 제작 중단

--- src/actions/cart-service/getProductByUuid.ts
+++ src/actions/cart-service/getProductByUuid.ts
@@ -5,32 +5,34 @@ import { getServerSession } from 'next-auth';
 
 export async function getCartProductByUuid(
   uuid: string
-): Promise<{ quantity: number; selected: boolean; valid: boolean }> {
+): Promise<{ quantity: number; selected: boolean }> {
   const session = await getServerSession(options);
   const accessToken = session?.user?.accessToken;
 
-  console.log(uuid);
-
-  const response = await fetch(`${process.env.API_BASE_URL}/cart/${uuid}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${accessToken}`,
-    },
-  });
+  const response = await fetch(
+    `${process.env.API_BASE_URL}/cart/by-product/${uuid}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    }
+  );
   if (!response.ok) {
     throw new Error('단 건 조회 데이터 패치 실패! 야외취침 확정!');
   }
 
   const data = await response.json();
-  return data.result;
+  return data[0];
 }
 
 export async function getInformationProductByUuid(uuid: string): Promise<{
   productName: string;
-  productPrice: string;
+  productPrice: number;
   productThumbnailUrl: string;
   isThumbnail: boolean;
+  shippingFee: number;
 }> {
   const session = await getServerSession(options);
   const accessToken = session?.user?.accessToken;
@@ -50,14 +52,31 @@ export async function getInformationProductByUuid(uuid: string): Promise<{
   }
 
   const data = await response.json();
+
   return data.result;
 }
 
 export async function removeItem(uuid: string) {
-  console.log(uuid);
-  await new Promise((resolve) => setTimeout(resolve, 500));
+  const session = await getServerSession(options);
+  const accessToken = session?.user?.accessToken;
 
-  return { success: true };
+  const response = await fetch(
+    `${process.env.API_BASE_URL}/cart/${uuid}/get-selected`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error('단 건 조회 데이터 패치 실패! 야외취침 확정!');
+  }
+
+  const data = await response.json();
+
+  return data.result.selected;
 }
 
 export async function updateQuantity(id: string, quantity: number) {
@@ -66,8 +85,25 @@ export async function updateQuantity(id: string, quantity: number) {
   return { success: true };
 }
 
-export async function ToggleCheckbox(uuid: string, checked: boolean) {
-  console.log(`[ToggleCheckbox] ${uuid} → ${checked}`);
-  await new Promise((res) => setTimeout(res, 200));
-  return { success: true };
+export async function ToggleCheckbox(uuid: string) {
+  const session = await getServerSession(options);
+  const accessToken = session?.user?.accessToken;
+
+  const response = await fetch(
+    `${process.env.API_BASE_URL}/cart/${uuid}/get-selected`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error('단 건 조회 데이터 패치 실패! 야외취침 확정!');
+  }
+
+  const data = await response.json();
+
+  return data.result.selected;
 }
