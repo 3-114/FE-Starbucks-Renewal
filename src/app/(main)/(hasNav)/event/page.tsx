efa95d2b0cc2704b4d68945cdf34272a184633cc
kimura <dnanf12345@gmail.com>
2025-04-22 16:45:32 +0900

    wip: 보안문제

--- src/app/(main)/(hasNav)/event/page.tsx
+++ src/app/(main)/(hasNav)/event/page.tsx
@@ -1,8 +1,6 @@
-import {
-  getEventNavData,
-  // getEventProductList,
-} from '@/actions/event-service/nav';
-// import { ProductList } from '@/components/feature/list/ProductList';
+import { getEventNavData } from '@/actions/event-service/nav';
+import EventNav from '@/components/layout/navs/EventNav';
+import { ProductList } from '@/components/feature/list/ProductList';
 import { EventNavMenuType } from '@/types/Initial/InitialDataTypes';
 
 export default async function Page({
@@ -10,16 +8,27 @@ export default async function Page({
 }: {
   searchParams: Promise<{ category?: string }>;
 }) {
-  const params = await searchParams;
-  const category = params.category ?? '';
+  const { category = '' } = await searchParams;
 
   const navData: EventNavMenuType[] = await getEventNavData();
 
-  const eventItem = navData.find((i) => i.eventName === category) || navData[0];
-  if (!eventItem) {
-    throw new Error(`유효하지 않은 카테고리입니다: ${category}`);
-  }
-  // const eventUuid = eventItem.eventUuid;
-  // const navListData: string[] = await getEventProductList(eventUuid);
-  return <>{/* <ProductList uuidList={navListData} /> */}</>;
+  const matchedCategory =
+    category === '' || category === '전체'
+      ? undefined
+      : navData.find((item) => item.eventName === category);
+
+  const eventUuid = matchedCategory?.eventUuid;
+
+  const data = await getEventProductList({
+    eventUuid,
+  });
+
+  const uuidList = data.content.map((item) => item.productUuid);
+
+  return (
+    <>
+      <EventNav NavData={navData} resetPageOnCategoryChange />
+      <ProductList uuidList={uuidList} showBest={false} />
+    </>
+  );
 }
